dc80a9c045d6806a34550230d74922e82809bd33
Christopher Fields <chrisjr06@gmail.com>
2021-06-23 23:56:57 -0500

    feat: create ListResponse type

--- src/response.ts
+++ src/response.ts
@@ -2,5 +2,10 @@ export function getListResponse<T>(items: T[]) {
   return {
     items,
     count: items.length,
-  }
+  };
+}
+
+export interface ListResponse<T = any[]> {
+  count: number;
+  items: T[];
 }
